9dfbd3cd8841450869a8f0d352b0c8b6b56e6762
Afif <afifhasan2003@gmail.com>
2025-05-27 19:42:56 +0600

    add npm project files

---
+++ guide.js
@@ -0,0 +1,11 @@
+//local dependency - use it only in this perticular project
+//npm i <package name>
+
+//global dependency - use it in any project
+//npm i -g <package name>
+//sudo npm install -g <package name> (for mac/linux)
+
+//package.json - file that keeps track of all the dependencies in your project
+//manual approach (create pakcage.json in the root, create properties etc)
+//npm init (step by step)
+//npm init -y (default values, no questions asked);
